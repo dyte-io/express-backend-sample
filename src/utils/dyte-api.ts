aad7e725c6e31eedd517efa4f05f2c7eb6a6420d
Vaibhav Shinde <vaibhav@dyte.io>
2022-11-29 14:52:13 +0530

    fix: package name changed, error message improved

--- src/utils/dyte-api.ts
+++ src/utils/dyte-api.ts
@@ -8,7 +8,7 @@ const DYTE_ORG_ID = process.env.DYTE_ORG_ID!;
 
 if (!DYTE_API_KEY || !DYTE_ORG_ID) {
   throw new Error(
-    'Samples: DYTE_API_KEY and DYTE_ORG_ID environment variables have not been set'
+    '❌ Backend Sample: DYTE_API_KEY and DYTE_ORG_ID environment variables have not been set'
   );
 }
 
